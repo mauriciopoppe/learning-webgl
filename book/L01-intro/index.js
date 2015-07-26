d03e5a331a211b5ab7164f7e00a61a418ceea951
Mauricio Poppe <me@maurizzzio.com>
2015-07-26 03:46:25 -0400

    initial commit

---
+++ book/L01-intro/index.js
@@ -0,0 +1,33 @@
+var twgl = require('twgl.js').twgl
+// program
+// - get the canvas
+// - get the rendering context for WebGL
+// - initialize shaders
+// - clear the canvas
+// - draw
+var canvas = document.querySelector('canvas')
+var gl = twgl.getWebGLContext(canvas)
+
+var VSHADER_SOURCE = `
+  void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 10.0;
+  }
+`
+
+var FSHADER_SOURCE = `
+  void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  }
+`
+
+var program = twgl.createProgramFromSources(gl, [VSHADER_SOURCE, FSHADER_SOURCE])
+gl.useProgram(program)
+
+gl.clearColor(0.0, 0.0, 0.0, 1.0)
+gl.clear(gl.COLOR_BUFFER_BIT)
+
+// execute the vertex shader in the mode of POINTS
+// from the vertex 0 using 1 vertex
+// mode, first, count
+gl.drawArrays(gl.POINTS, 0, 1)
---
+++ index.js
@@ -0,0 +1,50 @@
+var http = require('http')
+var handler = require('ecstatic')(process.cwd())
+var chokidar = require('chokidar')
+var spawn = require('npm-execspawn')
+var path = require('path')
+var inject = require('inject-lr-script')
+
+// live reload server
+var tinylr = require('tiny-lr')
+var lrServer = tinylr()
+lrServer.listen(35729, function () {
+  console.log('... LiveReload Listening on %s ...', 35729)
+})
+
+// static server
+http.createServer(function (req, res) {
+  return handler(req, inject(res))
+}).listen(9696)
+console.log('listening on http://localhost:9696')
+
+function runBrowserify (entry) {
+  var dest = path.dirname(entry) + '/bundle.js'
+  var cmd = ['browserify', entry,
+    '-d',
+    '-t', 'babelify',
+    '-o', dest
+  ].join(' ')
+  console.log('running: ' + cmd)
+  var proc = spawn(cmd)
+  proc.stdout.pipe(process.stdout)
+  proc.on('close', function () {
+    console.log('finished writing: ' + dest)
+    lrServer.changed({
+      body: {
+        files: '*'
+      }
+    })
+  })
+}
+
+var log = console.log.bind(console)
+chokidar
+  .watch('book/**/index.js')
+  .on('add', runBrowserify)
+  .on('change', function (file) {
+    log('changed: ' + file)
+    runBrowserify(file)
+  })
+  .on('error', function (error) { log('Error happened', error) })
+  .on('ready', function () { log('Initial scan complete. Ready for changes.') })
